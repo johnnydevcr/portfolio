289a531bf94ec8b0608a6482b2ed51a69452aacb
Johnny Barrantes <jbarrantes@gmail.com>
2020-08-17 20:38:28 -0600

    photo change

--- components/VHero.vue
+++ components/VHero.vue
@@ -1,12 +1,12 @@
 <template>
   <div
     id="hero"
-    class="bg-grey-dark shadow-md py-2 flex flex-col justify-center items-center sm:flex-row sm:justify-between sm:px-24 sm:py-4"
+    class="bg-grey-dark shadow-md py-2 items-center flex flex-row justify-between sm:px-24 sm:py-4"
   >
-    <div id="photo" class="w-1/3 sm:w-1/4 lg:w-1/12">
+    <div id="photo" class="w-1/3 ml-1 sm:ml-0 sm:w-2/12">
       <img class="rounded-full" src="@/assets/img/photo.jpg" alt="" />
     </div>
-    <div id="welcome" class=" text-white lg:w-10/12">
+    <div id="welcome" class="ml-2 sm:ml-0 text-white w-2/3 sm:w-9/12">
       <p class="text-2xl lg:text-5xl">
         Hello!
         <br />
