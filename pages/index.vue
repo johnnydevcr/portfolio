8c99a262c37cb7bfaf43b3af0d977f767b9d0b14
Johnny Barrantes <jbarrantes@gmail.com>
2020-08-19 10:56:50 -0600

    about me content

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,123 @@
 <template>
-  <div>
-    <h1>About Me</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, officiis
-      rem est veritatis commodi, nihil, nam nobis dolorum earum adipisci fugiat
-      magnam provident ex. Eius voluptatem repellendus ipsum dignissimos illo.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, officiis
-      rem est veritatis commodi, nihil, nam nobis dolorum earum adipisci fugiat
-      magnam provident ex. Eius voluptatem repellendus ipsum dignissimos illo.
-    </p>
+  <div
+    id="container"
+    class="flex flex-col items-center lg:flex-row lg:justify-between lg:items-start"
+  >
+    <section
+      id="information"
+      class="lg:w-3/5"
+    >
+      <h1 class="font-bold text-2xl">I'm Johnny Barrantes</h1>
+      <h3 class="font-bold text-xl text-red">Computer Engineer</h3>
+      <p class="text-justify my-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, officiis
+        rem est veritatis commodi, nihil, nam nobis dolorum earum adipisci fugiat
+        magnam provident ex. Eius voluptatem repellendus ipsum dignissimos illo.
+      </p>
+      <ul>
+        <li class="font-bold">Date of Birth: <span class="font-normal text-sm">22 february 1995</span></li>
+        <li class="font-bold">Phone Number: <span class="font-normal text-sm">+506 8747-3361</span></li>
+        <li class="font-bold">Address: <span class="font-normal text-sm">Alajuela, Costa Rica</span></li>
+        <li class="font-bold">Website: <span class="font-normal text-sm">www.johnnydevcr.com</span></li>
+      </ul>
+    </section>
+    <section
+      id="skills"
+      class="mt-8 lg:mt-0 lg:ml-24 w-full lg:w-2/5"
+    >
+      <h2 class="font-bold text-xl mb-4">My Skills</h2>
+      <div
+        id="box-set"
+        class="flex flex-row justify-around flex-wrap "
+      >
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+      </div>
+      <h2 class="font-bold text-xl my-4">My Knowledge</h2>
+      <div
+        id="box-set"
+        class="flex flex-row justify-around flex-wrap "
+      >
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+        <div
+          id="box"
+          class="w-5/12 my-2 flex flex-col items-center justify-center py-4 px-4 bg-red shadow-material-1 text-white"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'laptop-code']"
+            class="text-2xl lg:text-lg"
+          />
+          <span class="my-1 lg:my-0 lg:ml-2">Projects</span>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -18,4 +125,8 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.box {
+  font-family: "Lato", sans-serif;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,10 @@ var colors = {
 module.exports = {
   theme: {
     extend: {
+      boxShadow: {
+        'material-1': '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        'material-2': '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+      },
       colors: colors
     }
   },
