729eda0c072f99aa1debaecf270fd0d6fb4694cd
Johnny Barrantes <jbarrantes@gmail.com>
2020-08-19 11:44:45 -0600

    hero update

--- components/VHero.vue
+++ components/VHero.vue
@@ -3,16 +3,26 @@
     id="hero"
     class="bg-grey-dark shadow-md py-2 items-center flex flex-row justify-between sm:px-24 sm:py-4"
   >
-    <div id="photo" class="w-1/3 ml-1 sm:ml-0 sm:w-2/12">
-      <img class="rounded-full" src="@/assets/img/photo.jpg" alt="" />
+    <div
+      id="photo"
+      class="w-1/3 ml-1 sm:ml-0 sm:w-2/12"
+    >
+      <img
+        class="rounded-full"
+        src="@/assets/img/photo.jpg"
+        alt=""
+      />
     </div>
-    <div id="welcome" class="ml-2 sm:ml-0 text-white w-2/3 sm:w-9/12">
+    <div
+      id="welcome"
+      class="ml-2 sm:ml-0 text-white w-2/3 sm:w-9/12"
+    >
       <p class="text-2xl lg:text-5xl">
         Hello!
         <br />
         I'm <span class="text-red">Johnny Barrantes</span>
       </p>
-      <p class="font-normal lg:text-3xl">Computer Engineer</p>
+      <p class="font-normal lg:text-3xl">Web Developer</p>
     </div>
   </div>
 </template>
